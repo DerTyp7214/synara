fc2cd7b7539bca91fe601d0d4ea7f8acaa01f0e3
YungCatz <72994063+YungCatz@users.noreply.github.com>
2025-12-16 23:51:08 +0000

    Deploying to gh-pages from @ DerTyp7214/synara@2dfe158966c11fd246be0e075c7938c1dfab0732 🚀

---
+++ _app/immutable/chunks/B-qtRsA9.js
@@ -0,0 +1,1 @@
+import{b as p,E as t}from"./BDnkRP4C.js";import{B as c}from"./zxQDN27n.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
---
+++ _app/immutable/chunks/Cwzcqwzn.js
@@ -0,0 +1,1 @@
+import{k as a}from"./BDnkRP4C.js";a();
