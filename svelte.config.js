8c6da2b70a8d2dbae0c02d37509a9d264b00c19f
DerBot7214 <github@dertyp.dev>
2026-01-29 00:52:23 +0100

    update github actions; fix .nojekyll typo; reformat configs

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from "@sveltejs/adapter-static";
 
 export default {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
     kit: {
         adapter: adapter({
             fallback: '404.html'
